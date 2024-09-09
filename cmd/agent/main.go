946171c1c7e4c2e7fcf2cf32fa6a675b261fdda8
Andrei Markov <markov.andrey.v@gmail.com>
2024-09-09 10:29:21 +0200

    ITER20: Add source code for the iter20

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,4 @@
+//nolint:gochecknoglobals
 package main
 
 import (
@@ -7,7 +8,15 @@ import (
 	"github.com/andymarkow/go-metrics-collector/internal/agent"
 )
 
+var (
+	buildVersion = "N/A" // Build version number.
+	buildDate    = "N/A" // Build creation date.
+	buildCommit  = "N/A" // Build commit hash.
+)
+
 func main() {
+	printBuildInfo()
+
 	agnt, err := agent.NewAgent()
 	if err != nil {
 		log.Fatal(fmt.Errorf("agent.NewAgent: %w", err))
@@ -16,4 +25,11 @@ func main() {
 	if err := agnt.Start(); err != nil {
 		log.Fatal(fmt.Errorf("agent.Start: %w", err))
 	}
+}
+
+// printBuildInfo prints the build version, date, and commit hash.
+func printBuildInfo() {
+	log.Println("Build version:", buildVersion)
+	log.Println("Build date:", buildDate)
+	log.Println("Build commit:", buildCommit)
 }
